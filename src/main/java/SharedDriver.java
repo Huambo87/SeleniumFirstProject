45d68f9f5d22a03bd11a0128758de904276e54c3
Huambo87 <maximgladkov@hotmail.com>
2024-07-04 19:18:28 -0400

    Selenium Examples

--- src/main/java/SharedDriver.java
+++ src/main/java/SharedDriver.java
@@ -1,6 +1,7 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class SharedDriver {
 
@@ -12,6 +13,9 @@ public class SharedDriver {
 //            WebDriverManager.chromedriver().clearDriverCache();
             WebDriverManager.chromedriver().setup();
             webDriver = new ChromeDriver();
+
+//            WebDriverManager.firefoxdriver().setup();
+//            webDriver = new FirefoxDriver();
         }
         return webDriver;
     }
